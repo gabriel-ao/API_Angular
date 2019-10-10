b0288b5d23ec5994bf29320014efb2d5c98d2296
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2019-10-10 17:01:22 -0300

    Trabalhando com Entidades
    Entidades, Enum entre outros alinhamentos

---
+++ QuickBuy/QuickBuy/Entidades/Pedido.cs
@@ -0,0 +1,34 @@
+﻿using QuickBuy.Domain.ObjectValue;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string EnderecoCompleto { get; set; }
+
+        public string NumeroEndereco { get; set; }
+
+        public FormaPagamento FormaPagamento { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
---
+++ QuickBuy/QuickBuy/ObjectValue/FormaPagamento.cs
@@ -0,0 +1,38 @@
+﻿using QuickBuy.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.ObjectValue
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto;  }
+        }
+
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+
+        public bool NaoFoiDEfinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+
+
+    }
+}
